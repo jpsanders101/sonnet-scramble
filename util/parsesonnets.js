1463283d37a7c5456522984b12e25870aed41d15
jpsanders101 <jpsanders101@gmail.com>
2020-03-28 12:54:54 +0000

    Add script for reformatting sonnet jsons.

---
+++ util/parsesonnets.js
@@ -0,0 +1,15 @@
+const fs = require('fs');
+
+let sonnets = fs.readFileSync('./src/pages/sonnets/all-sonnets.json', 'utf-8');
+JSON.parse(sonnets).forEach((sonnetObj, index) => {
+  const newLines = sonnetObj.lines.map((line, index) => ({
+    lineNumber: index + 1,
+    line
+  }))
+
+  const newSonnet = {
+    title: `${index + 1}`,
+    lines: newLines
+  };
+  fs.writeFileSync(`./src/pages/sonnets/${index+1}.json`, JSON.stringify(newSonnet), {options: 'utf-8'})
+})
