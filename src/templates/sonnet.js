3040578d04e341cd2ea147fbc4ce34043092840f
jpsanders101 <jpsanders101@gmail.com>
2020-03-28 13:27:09 +0000

    Create two columns for lines and solution.

--- src/templates/sonnet.js
+++ src/templates/sonnet.js
@@ -2,13 +2,48 @@ import React from "react"
 import { Link, graphql } from "gatsby"
 import { css } from "@emotion/core"
 
-export default ({ data }) => {  
+export default ({ data }) => {
+  const { sonnetsJson: { lines }} = data;  
   return (
     <main>
       <h1>{`Sonnet Page`}</h1>
-      {data.sonnetsJson.lines.map(lineNode => (
-        <p css={line}>{lineNode.line}</p>
-      ))}
+      <section css={puzzleSection}>
+        <div>
+          <div css={solvedLine}>
+          </div>
+          <div css={solvedLine}>
+          </div>
+          <div css={solvedLine}>
+          </div>
+          <div css={solvedLine}>
+          </div>
+          <div css={solvedLine}>
+          </div>
+          <div css={solvedLine}>
+          </div>
+          <div css={solvedLine}>
+          </div>
+          <div css={solvedLine}>
+          </div>
+          <div css={solvedLine}>
+          </div>
+          <div css={solvedLine}>
+          </div>
+          <div css={solvedLine}>
+          </div>
+          <div css={solvedLine}>
+          </div>
+          <div css={solvedLine}>
+          </div>
+          <div css={solvedLine}>
+          </div>
+        </div>
+        <div>
+          {lines.map(lineNode => (
+            <p css={line}>{lineNode.line}</p>
+          ))}
+        </div>
+      </section>
       <Link to={`/${parseInt(data.sonnetsJson.title) + 1}`}>{`Next`}</Link>
     </main>
   )
@@ -16,6 +51,18 @@ export default ({ data }) => {
 
 const line = css`
   color: red;
+`;
+
+const solvedLine = css`
+  height: 30px;
+  width: 80%;
+  border: solid hotpink 3px;
+  margin: 10px;
+`;
+
+const puzzleSection = css`
+  display: grid;
+  grid-template-columns: 1fr 1fr
 `;
 
 export const query = graphql`
