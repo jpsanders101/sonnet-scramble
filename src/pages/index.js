e8811e20618b633feaf2afccd3aaccfd140fae10
jpsanders101 <jpsanders101@gmail.com>
2020-03-28 18:17:01 +0000

    Update README.

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,6 +3,8 @@ import { Link } from "gatsby"
 
 export default () => <main>
   <h1>{`Sonnet Scramble`}</h1>
-  <p>{`Can you unscramble all 154 of Shakespeare’s sonnets?`}</p>
+  <p>{`At several times during Shakespeare’s career, the London theatres which were his main livelihood were closed due to outbreaks of the plague. It’s a possible reason why he turned his hand to writing poems that readers could enjoy in privacy when public life became dangerous.`}</p>
+  <p>{`A book of 154 Sonnets was first published in 1609.`}</p>
+  <p>{`Here, they are all scrambled. Can you unscramble them?`}</p>
   <Link to="/1">{`Begin`}</Link>
 </main>
