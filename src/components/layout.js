6238cdc5f12e655e9ec4bc81fc02e30073c3d437
Jon Sanders <jpsanders101@gmail.com>
2020-05-19 19:23:04 +0100

    Move background colour to global body.

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,13 +1,10 @@
 import React from "react";
 import { css } from "@emotion/core"
-import { OFF_WHITE } from "../constants/styles.js";
 import "../../assets/global.css";
 
 const Layout = ({ children }) => <main css={layoutStyle}>{children}</main>
 
 const layoutStyle = css`
-  background-color: ${OFF_WHITE};
-  height: 100vh;
   padding: 5px;
   & * {
     font-family: Palatino, Georgia, serif;
